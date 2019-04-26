e7cf1fd4c08a75951e2de386a6ea59b6a220a51f
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2019-04-26 16:50:46 +0100

    pkgify, fix dendrograms (mostly)

--- R/crayonmaps.R
+++ R/crayonmaps.R
@@ -1,3 +1,4 @@
+# Set the crayonmap class. Still not very useful but, since the crayons slot is a vector, it is important to memorize the number of rows and columns for future applications
 setClass("crayonmap", representation(crayons = "character", row.num = "numeric", col.num = "numeric"))
 
 #' Color key
@@ -19,11 +20,21 @@ colorKey <- function(values,
   return(cols)
 } 
 
-paintCrayon <- function(cmap) {   
-  if(!is(cmap, "crayonmap")) stop("Must supply a crayonmap object.")
-  cat(cmap@crayons, sep = "")
-}
+setMethod("show", "crayonmap", function(object) cat(object@crayons, sep = ""))
 
+rpad <- function(str, width = max(nchar(str))) {
+  if (max(nchar(str)) > width) {
+    str <- substr(str, 0, width)
+  }
+  mn <- max(nchar(str))
+  paste0(
+    str, 
+    vapply(mn - nchar(str), 
+      function(i) paste(rep(" ", i), collapse = ""),
+      character(1)
+    )
+  )
+}
 
 #' Text-based heatmap,
 #' @param dat matrix with numeric values, rownames and colnames
@@ -36,7 +47,6 @@ paintCrayon <- function(cmap) {
 #' @param key character, title of the color legend
 #' @param show_col_legend logical, should the column legend be shown at the end of the plot? default is TRUE
 #' @return a text-based heatmap directly in the terminal output using ANSI background styles. Useful when your X11 forwarding is broken or for quick exploratory analysis of small datasets. Setup is largely inspired by pheatmap.
-
 textHeatmap <-function(dat, 
     pal = colorRampPalette(c("red", "gray", "blue"))(24), 
     cluster_cols = TRUE, 
@@ -45,13 +55,15 @@ textHeatmap <-function(dat,
     clustering_method = "complete",
     main = "Heatmap",
     key = "Key", 
-    show_col_legend = TRUE) {
+    show_col_legend = TRUE,
+    lpad = "") {
 
   require(crayon)
 
-  #Clustering
+  ## Clustering
+  ## Hierarchical clustering of the rows
   if (cluster_rows) { 
-    hr <- hclust(                #Hierarchical clustering of the rows
+    hr <- hclust(                
       dist(dat, method = dist_method), 
       method = clustering_method
     )
@@ -59,9 +71,10 @@ textHeatmap <-function(dat,
     hr <- list("order" = 1:nrow(dat))
   }
 
+  ## Hierarchical clustering of columns (same as rows but transposing the matrix)
   if (cluster_cols) {
     hc <- hclust(
-      dist(t(dat), method = dist_method),  #Hierarchical clustering of columns (same as rows but transposing the matrix)
+      dist(t(dat), method = dist_method),  
       method = clustering_method
     )
   } else {
@@ -70,7 +83,7 @@ textHeatmap <-function(dat,
 
   dat <- dat[hr$order, hc$order]
 
-  #Colour rendering and crayonmap object
+  ## Colour rendering and crayonmap object
   crayonstrings <- matrix(
     unlist(
       sapply(colorKey(as.vector(dat), pal = pal), 
@@ -81,13 +94,27 @@ textHeatmap <-function(dat,
     ), 
     nrow = nrow(dat)
   )
-  crayonstrings[, ncol(crayonstrings)] <- paste(
+  crayonstrings[, ncol(crayonstrings)] <- paste0(
     crayonstrings[, ncol(crayonstrings)], 
     crayon::reset("   "), 
-    rownames(dat), "\n", sep = ""
+    rpad(rownames(dat))
   )
 
-  crayonstrings[, 1] <- paste("   ", crayonstrings[, 1], sep = "")
+  crayonstrings[, 1] <- paste(lpad, crayonstrings[, 1], sep = "")
+
+  if (cluster_rows) {
+    rd <- textDendrogram(hr, horiz = TRUE, yend = 20)
+    crayonstrings <- cbind(crayonstrings, rd)
+    crayonstrings[, ncol(crayonstrings)] <- paste0(
+      crayonstrings[, ncol(crayonstrings)]
+    )    
+  } else {
+    crayonstrings[, ncol(crayonstrings)] <- paste0(
+      crayonstrings[, ncol(crayonstrings)],
+      "\n"
+    )
+  }
+
   crayons <- as.vector(t(crayonstrings))
 
   cmap <- new("crayonmap", 
@@ -95,14 +122,13 @@ textHeatmap <-function(dat,
     row.num = nrow(dat), 
     col.num = ncol(dat))
 
-  # Column names
+  ## Column dendrogram
   if (cluster_cols) {
-    cat(textDendrogram(hc), sep = "")
+    cat(as.vector(t(textDendrogram(hc, yend = 10))), sep = "")
   }
-
-  #padding
-  cat("\n \n")
-  cat("   ")
+  ## Column names
+  ## padding
+  cat(lpad)
 
   for (k in seq_len(ncol(dat))) {
     if (nchar(k) == 1) {
@@ -113,13 +139,13 @@ textHeatmap <-function(dat,
   }
   cat("\n")
 
-  #Plot the heatmap
-  paintCrayon(cmap)
+  ## Plot the heatmap
+  print(cmap)
 
-  #Color Key
-  #padding
-  cat("\n \n") 
-  cat("   ")
+  ## Color Key
+  ## padding
+  # cat("\n \n") 
+  cat(lpad)
   ckey <- unique(
     unlist(
       sapply(pal, 
@@ -131,7 +157,8 @@ textHeatmap <-function(dat,
   )
   mid.value <- round((max(dat) - abs(min(dat))) / 2)
 
-  cat("Key", "\n", "   ", 
+  cat("Key", "\n", 
+    lpad, 
     round(min(dat)), 
     rep("   ", length(ckey) / 2), 
     mid.value,  
@@ -141,34 +168,52 @@ textHeatmap <-function(dat,
     sep = ""
   )
 
-  #Column legend
+  ## Column legend
   if (show_col_legend) {
-    cat("   ")
+    cat(lpad)
     cat("Column legend:\n")
     for (i in seq_len(ncol(dat))) {
-      cat(paste("   ", i, ":", colnames(dat)[i], "\n", sep = ""))
+      cat(paste(lpad, i, ":", colnames(dat)[i], "\n", sep = ""))
     }
   }
 }
 
 
-textDendrogram <- function(hc) {
-  require("dendextend")
+textDendrogram <- function(hc, horiz = FALSE, yend = 20) {
   d <- as.dendrogram(hc, hang = -1)
-  ggd <- as.ggdend(d)
+  ggd <- dendextend::as.ggdend(d)
   segs <- ggd$segments
-  segs[, c("y", "yend")] <- lapply(segs[, c("y", "yend")], scales::rescale, to = c(20, 0))
-  segs[, c("x", "xend")] <- round(segs[, c("x", "xend")] * 3)
+  xmax <- max(segs$x)
+  yscale <- c(1, yend)
+  segs[, c("y", "yend")] <- lapply(segs[, c("y", "yend")], 
+    scales::rescale, to = if (horiz) yscale else rev(yscale)
+  )
+  if (!horiz) {
+    segs[, c("x", "xend")] <- round(segs[, c("x", "xend")] * 3)
+  }
+  
+  if (horiz) {
+    segs[, c("x", "xend")] <- lapply(segs[, c("x", "xend")], 
+      function(x) ifelse(x < xmax, x, xmax)
+    )
+  }
   segs[, c("x", "y", "xend", "yend")] <- round(segs[, c("x", "y", "xend", "yend")])
   mat <- matrix(NA, ncol = max(segs$x), nrow = max(segs$y))
+  if (horiz) {
+    mat <- t(mat)
+    segs[, c("x", "xend")] <- segs[, c("x", "xend")] + segs[, c("y", "yend")]
+    segs[, c("y", "yend")] <- segs[, c("x", "xend")] - segs[, c("y", "yend")]
+    segs[, c("x", "xend")] <- segs[, c("x", "xend")] - segs[, c("y", "yend")]    
+  }
   for (i in seq_len(nrow(segs))) {
     mat <- add_text_line(mat, segs[i, "x"], segs[i, "xend"], segs[i, "y"], segs[i, "yend"])
   }
-  mat[is.na(mat)] <- crayon::make_style("black", bg = TRUE)(" ")
+  mat[is.na(mat)] <- " "
   mat[, ncol(mat)] <- paste(mat[, ncol(mat)], crayon::reset(), "\n")
-  mat[, 1] <- paste("   ", mat[, 1], sep = "")
-  crayons <- as.vector(t(mat))
-  crayons
+  if (!horiz) {
+    mat[, 1] <- paste("   ", mat[, 1], sep = "")
+  }
+  mat
 }
 
 add_text_line <- function(matrix, x, xend, y, yend) {
